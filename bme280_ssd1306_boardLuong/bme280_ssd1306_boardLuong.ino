8e139eb355909d5c5ccbf3f91c3d137b65999edd
Minh Duc <ducpt423ac@gmail.com>
2026-06-04 12:08:37 +0700

    test commit from chatbot

--- bme280_ssd1306_boardLuong/bme280_ssd1306_boardLuong.ino
+++ bme280_ssd1306_boardLuong/bme280_ssd1306_boardLuong.ino
@@ -33,7 +33,7 @@ const char* pass = "04072009";
 #define LED_COUNT 1
 Adafruit_NeoPixel led(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#define LED 2
+#define LED 2                     // chân LED nhấp nháy
 
 #define i2c_Address 0x3c
 #define SCREEN_WIDTH 128
@@ -51,7 +51,7 @@ FirebaseConfig config;
 
 int checkupdate = 0;
 
-// Biến toàn cục cho việc nhấp nháy LED
+// biến cho việc nhấp nháy LED
 unsigned long previousMillis = 0;
 const unsigned long interval = 2000; // 2 giây
 bool ledState = false;
@@ -69,68 +69,67 @@ void getupdate()
 
     if (httpCode == HTTP_CODE_OK)
     {
-      WiFiClient& client = http.getStream();
-      int firmwareSize = http.getSize();
-      display.clearDisplay();
-      display.setTextSize(1);
-      Serial.print("Firmware Size: ");
-      Serial.println(firmwareSize);
-      display.setCursor(0, 0);
-      display.printf("Size: %d",firmwareSize);
-      display.display();
-      if (Update.begin(firmwareSize))
-      {
-          Update.onProgress([](size_t current, size_t total) {
-              int percent = (current * 100) / total;
+        WiFiClient& client = http.getStream();
+        int firmwareSize = http.getSize();
+        display.clearDisplay();
+        display.setTextSize(1);
+        Serial.print("Firmware Size: ");
+        Serial.println(firmwareSize);
+        display.setCursor(0, 0);
+        display.printf("Size: %d",firmwareSize);
+        display.display();
+        if (Update.begin(firmwareSize))
+        {
+            Update.onProgress([](size_t current, size_t total) {
+                int percent = (current * 100) / total;
 
-              Serial.printf("OTA %d%%
-", percent);
+                Serial.printf("OTA %d%%\n", percent);
 
-              display.clearDisplay();
-              display.setCursor(0,0);
-              display.print("Updating");
+                display.clearDisplay();
+                display.setCursor(0,0);
+                display.print("Updating");
 
-              display.setCursor(0,20);
-              display.print(percent);
-              display.print("%");
-              display.drawRect(0, 30, 120, 10, SSD1306_WHITE);
-              display.fillRect(
-                    2,
-                    32,
-                    (percent * 116) / 100,
-                    6,
-                    SSD1306_WHITE);
-              display.display();
-          });
-          size_t written = Update.writeStream(client);
-          display.clearDisplay();
-          if (Update.size() == written)
-          {
-              display.setCursor(0, 10);
-              display.print("Update successfully completed");
-              Serial.println("Update successfully completed. Rebooting...");
-              if (Update.end())
-              {
-                  Serial.println("Rebooting...");
-                  display.setCursor(0, 30);
-                  display.printf("Rebooting...");
-                  ESP.restart();
-              } 
-              else 
-              {
-                  Serial.print("Update failed: ");
-                  display.setCursor(0, 30);
-                  display.print("Update failed");
-                  Serial.println(Update.errorString());
-              }
-          }
-          else
-          {
-              display.setCursor(0, 30);
-              display.print("Not enough space for OTA.");
-              Serial.println("Not enough space for OTA.");
-          }
-      } 
+                display.setCursor(0,20);
+                display.print(percent);
+                display.print("%");
+                display.drawRect(0, 30, 120, 10, SSD1306_WHITE);
+                display.fillRect(
+                      2,
+                      32,
+                      (percent * 116) / 100,
+                      6,
+                      SSD1306_WHITE);
+                display.display();
+            });
+            size_t written = Update.writeStream(client);
+            display.clearDisplay();
+            if (Update.size() == written)
+            {
+                display.setCursor(0, 10);
+                display.print("Update successfully completed");
+                Serial.println("Update successfully completed. Rebooting...");
+                if (Update.end())
+                {
+                    Serial.println("Rebooting...");
+                    display.setCursor(0, 30);
+                    display.printf("Rebooting...");
+                    ESP.restart();
+                } 
+                else 
+                {
+                    Serial.print("Update failed: ");
+                    display.setCursor(0, 30);
+                    display.print("Update failed");
+                    Serial.println(Update.errorString());
+                }
+            }
+            else
+            {
+                display.setCursor(0, 30);
+                display.print("Not enough space for OTA.");
+                Serial.println("Not enough space for OTA.");
+            }
+        } 
         else
         {
             display.setCursor(0, 10);
@@ -152,104 +151,97 @@ void getupdate()
 }
 
 void setup() {
-  /*
-    Người dùng build code tại đây
-  */
-  // Khởi tạo trạng thái LED và biến thời gian
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, LOW);
-  ledState = false;
-  previousMillis = millis();
-
-  Wire.begin(8,18);
-  led.begin();
-  led.setBrightness(50);
-  led.setPixelColor(0, led.Color(255, 0, 255));
-  led.show();  
-  if (!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
-    led.setPixelColor(0, led.Color(255, 0, 0));
-    led.show();
-    Serial.println("OLED fail!");
-    while (1);
-  }
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.printf("He thong dang 
-khoi dong...");
-  display.display();
-  delay(1000);
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,0);
-  Serial.begin(115200);
-  Serial.println("He thong dang khoi dong...");
-  display.display();
-  display.clearDisplay();
-  WiFi.begin(ssid,pass);
-  while (WiFi.status() != WL_CONNECTED) {
+    /*
+      Người dùng build code tại đây
+    */
+    Wire.begin(8,18);
+    led.begin();
+    led.setBrightness(50);
     led.setPixelColor(0, led.Color(255, 0, 255));
-    led.show();
-    Serial.println("dang khoi dong WiFi...");
-    display.setCursor(0,0);
-    display.print("Conecting WiFi");
-    if(demwf < 80) {
-      display.setCursor(demwf,10);
-      display.print(".");
-      Serial0.println(".");
+    led.show();  
+    if (!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
+        led.setPixelColor(0, led.Color(255, 0, 0));
+        led.show();
+        Serial.println("OLED fail!");
+        while (1);
     }
-    else if(demwf > 80) {
-      display.clearDisplay();
-      demwf = 0;
-    }
-    demwf+=5;
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.printf("He thong dang khoi dong...");
     display.display();
-    digitalWrite(LED,1);
+    delay(1000);
+    pinMode(LED,OUTPUT);
+    digitalWrite(LED,LOW);               // LED bắt đầu tắt
+    Serial.begin(115200);
+    Serial.println("He thong dang khoi dong...");
+    display.display();
+    display.clearDisplay();
+    WiFi.begin(ssid,pass);
+    int demwf = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        led.setPixelColor(0, led.Color(255, 0, 255));
+        led.show();
+        Serial.println("dang khoi dong WiFi...");
+        display.setCursor(0,0);
+        display.print("Conecting WiFi");
+        if(demwf < 80) {
+            display.setCursor(demwf,10);
+            display.print(".");
+            Serial.println(".");
+        }
+        else if(demwf > 80) {
+            display.clearDisplay();
+            demwf = 0;
+        }
+        demwf+=5;
+        display.display();
+        digitalWrite(LED,HIGH);
+        delay(300);
+    }
+    digitalWrite(LED,LOW);
+    Serial.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
+    config.database_url = DATABASE_URL;
+    config.signer.tokens.legacy_token = DATABASE_SECRET;
+    Firebase.reconnectWiFi(true);
+    fbdo.setBSSLBufferSize(512, 512);
+    Firebase.begin(&config, &auth);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0, 30);
+    display.println("XIN CHAO CAC BAN");
+    display.display();
     delay(300);
-  }
-  digitalWrite(LED,0);
-  Serial.printf("Firebase Client v%s
-
-", FIREBASE_CLIENT_VERSION);
-  config.database_url = DATABASE_URL;
-  config.signer.tokens.legacy_token = DATABASE_SECRET;
-  Firebase.reconnectWiFi(true);
-  fbdo.setBSSLBufferSize(512, 512);
-  Firebase.begin(&config, &auth);
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setCursor(0, 30);
-  display.println("XIN CHAO CAC BAN");
-  display.display();
-  delay(300);
-  display.clearDisplay();
-  led.setPixelColor(0, led.Color(0, 255, 0));
-  led.show();
+    display.clearDisplay();
+    led.setPixelColor(0, led.Color(0, 255, 0));
+    led.show();
 }
 
 void loop() {
-  if(Firebase.getInt(fbdo, "/updateOTA")) checkupdate = fbdo.intData();
-  if(checkupdate == 1) {
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setCursor(0, 0);
-    display.print("UPDATE OTA");
-    display.display();
-    getupdate();
-  }
-  /*
-    Xây dựng cơ chế xử lý của bạn tại đây
-  */
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    ledState = !ledState;
-    digitalWrite(LED, ledState ? HIGH : LOW);
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setCursor(0,0);
-    display.print("LED ");
-    display.print(ledState ? "ON" : "OFF");
-    display.display();
-  }
+    if(Firebase.getInt(fbdo, "/updateOTA")) checkupdate = fbdo.intData();
+    if(checkupdate == 1) {
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setCursor(0, 0);
+        display.print("UPDATE OTA");
+        display.display();
+        getupdate();
+    }
+
+    /*
+      Xây dựng cơ chế xử lý của bạn tại đây
+    */
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        ledState = !ledState;
+        digitalWrite(LED, ledState ? HIGH : LOW);
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setCursor(0,0);
+        display.print("LED: ");
+        display.print(ledState ? "ON" : "OFF");
+        display.display();
+    }
 }
