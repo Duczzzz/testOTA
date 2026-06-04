9a68bc5d0bd4d7ab89eafea505f86db5a7a0b901
Minh Duc <ducpt423ac@gmail.com>
2026-06-04 10:43:16 +0700

    test commit from chatbot

--- bme280_ssd1306_boardLuong/bme280_ssd1306_boardLuong.ino
+++ bme280_ssd1306_boardLuong/bme280_ssd1306_boardLuong.ino
@@ -1,12 +1,223 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <FirebaseESP32.h>
+#include <WiFi.h>
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <HTTPClient.h>
+#include <Update.h>
+#include <Adafruit_Sensor.h>
+
+const char* ssid = "DUC";
+const char* pass = "14042004";
+
+#define LED_PIN   9
+#define LED_COUNT 1
+Adafruit_NeoPixel led(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+#define LED 2
+
+#define i2c_Address 0x3c
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+const char* firmwareUrl = "https://raw.githubusercontent.com/Duczzzz/testOTA/main/firmware.ino.bin";
+
+#define DATABASE_URL "https://doantn-885dc-default-rtdb.firebaseio.com/"
+#define DATABASE_SECRET "rPb2lv5DjHze997hD9pxnzTzWJsir4wwdP1poStt"
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+int checkupdate = 0;
+
+// Biến cho việc nhấp nháy LED
+unsigned long previousMillis = 0;
+const unsigned long interval = 2000; // 2 giây
+bool ledState = false;
+
+void getupdate()
+{
+    display.setTextColor(SSD1306_WHITE);
+    Firebase.setInt(fbdo, "/updateOTA",0);  
+    Serial.print("Firmware URL: ");
+    Serial.println(firmwareUrl);
+    HTTPClient http;
+    http.begin(firmwareUrl);
+    Firebase.setInt(fbdo,"/updateOTA",0);
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK)
+    {
+        WiFiClient& client = http.getStream();
+        int firmwareSize = http.getSize();
+        display.clearDisplay();
+        display.setTextSize(1);
+        Serial.print("Firmware Size: ");
+        Serial.println(firmwareSize);
+        display.setCursor(0, 0);
+        display.printf("Size: %d",firmwareSize);
+        display.display();
+        if (Update.begin(firmwareSize))
+        {
+            Update.onProgress([](size_t current, size_t total) {
+                int percent = (current * 100) / total;
+
+                Serial.printf("OTA %d%%\n", percent);
+
+                display.clearDisplay();
+                display.setCursor(0,0);
+                display.print("Updating");
+                display.setCursor(0,20);
+                display.print(percent);
+                display.print("%");
+                display.drawRect(0, 30, 120, 10, SSD1306_WHITE);
+                display.fillRect(2, 32, (percent * 116) / 100, 6, SSD1306_WHITE);
+                display.display();
+            });
+            size_t written = Update.writeStream(client);
+            display.clearDisplay();
+            if (Update.size() == written)
+            {
+                display.setCursor(0, 10);
+                display.print("Update successfully completed");
+                Serial.println("Update successfully completed. Rebooting...");
+                if (Update.end())
+                {
+                    Serial.println("Rebooting...");
+                    display.setCursor(0, 30);
+                    display.printf("Rebooting...");
+                    ESP.restart();
+                } 
+                else 
+                {
+                    Serial.print("Update failed: ");
+                    display.setCursor(0, 30);
+                    display.print("Update failed");
+                    Serial.println(Update.errorString());
+                }
+            }
+            else
+            {
+                display.setCursor(0, 30);
+                display.print("Not enough space for OTA.");
+                Serial.println("Not enough space for OTA.");
+            }
+        } 
+        else
+        {
+            display.setCursor(0, 10);
+            display.print("Failed to begin OTA update.");
+            Serial.println("Failed to begin OTA update.");
+        }
+    }
+    else
+    {
+        display.setCursor(0, 10);
+        display.print("Failed to download firmware. HTTP code: ");
+        display.println(httpCode);
+        Serial.print("Failed to download firmware. HTTP code: ");
+        Serial.println(httpCode);
+    }
+    display.display();
+    delay(400);
+    http.end();
+}
 
 void setup() {
-    pinMode(2, OUTPUT);
+    // Khởi tạo I2C, LED NeoPixel, OLED và hiển thị trạng thái LED ban đầu
+    Wire.begin(8,18);
+    led.begin();
+    led.setBrightness(50);
+    led.setPixelColor(0, led.Color(255, 0, 255));
+    led.show();  
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
+        led.setPixelColor(0, led.Color(255, 0, 0));
+        led.show();
+        Serial.println("OLED fail!");
+        while (1);
+    }
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.printf("He thong dang khoi dong...");
+    display.display();
+    delay(1000);
+
+    pinMode(LED,OUTPUT);
+    digitalWrite(LED,0);
+    Serial.begin(115200);
+    Serial.println("He thong dang khoi dong...");
+    display.display();
+    display.clearDisplay();
+
+    WiFi.begin(ssid,pass);
+    int demwf = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+        led.setPixelColor(0, led.Color(255, 0, 255));
+        led.show();
+        Serial.println("dang khoi dong WiFi...");
+        display.setCursor(0,0);
+        display.print("Conecting WiFi");
+        if(demwf < 80) {
+            display.setCursor(demwf,10);
+            display.print(".");
+            Serial.println(".");
+        }
+        else if(demwf > 80) {
+            display.clearDisplay();
+            demwf = 0;
+        }
+        demwf+=5;
+        display.display();
+        digitalWrite(LED,1);
+        delay(300);
+    }
+    digitalWrite(LED,0);
+    Serial.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
+    config.database_url = DATABASE_URL;
+    config.signer.tokens.legacy_token = DATABASE_SECRET;
+    Firebase.reconnectWiFi(true);
+    fbdo.setBSSLBufferSize(512, 512);
+    Firebase.begin(&config, &auth);
+
+    // Hiển thị trạng thái LED ban đầu trên OLED
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setCursor(0, 30);
+    display.println("LED OFF");
+    display.display();
+    delay(300);
+    display.clearDisplay();
+    led.setPixelColor(0, led.Color(0, 255, 0));
+    led.show();
 }
 
 void loop() {
-    digitalWrite(2, HIGH);
-    delay(500);
-    digitalWrite(2, LOW);
-    delay(500);
+    if(Firebase.getInt(fbdo, "/updateOTA")) checkupdate = fbdo.intData();
+    if(checkupdate == 1) {
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setCursor(0, 0);
+        display.print("UPDATE OTA");
+        display.display();
+        getupdate();
+    }
+
+    // Nhấp nháy LED mỗi 2 giây và cập nhật trạng thái lên OLED
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        ledState = !ledState;
+        digitalWrite(LED, ledState ? HIGH : LOW);
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setCursor(0, 0);
+        display.print(ledState ? "LED ON" : "LED OFF");
+        display.display();
+    }
 }
