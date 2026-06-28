b507fb4baa11cd44a3805754c31850b6896d9a46
Minh Duc <ducpt423ac@gmail.com>
2026-06-28 20:50:11 +0700

    build-1782654612287:luong

--- luong/luong.ino
+++ luong/luong.ino
@@ -143,6 +143,9 @@ void getupdate()
 }
 
 void setup() {
+  /*
+    Người dùng build code tại đây
+  */
   I2C_BME.begin(8,18);
   I2C_OLED.begin(13,12);
   led.begin();
@@ -232,27 +235,31 @@ void loop() {
     display.display();
     getupdate();
   }
-  float temperature = bme.readTemperature();
-  float humidity = bme.readHumidity();
+  /*
+    Xây dựng cơ chế xử lý của bạn tại đây
+  */
+  float temp = bme.readTemperature();
+  float hum = bme.readHumidity();
   display.clearDisplay();
+  display.setTextSize(1);
   display.setCursor(0,0);
-  display.print("Nhiet do: ");
-  display.print(temperature);
-  display.print((char)247);
+  display.print("Temp: ");
+  display.print(temp);
+  display.print((char)176);
   display.print("C");
   display.setCursor(0,10);
-  display.print("Do am: ");
-  display.print(humidity);
+  display.print("Hum: ");
+  display.print(hum);
   display.print("%");
   display.display();
-  if(temperature>36){
+  if (temp>36)
+  {
     led.setPixelColor(0, led.Color(255,0,0));
-  }else{
+  }
+  else
+  {
     led.setPixelColor(0, led.Color(0,255,0));
   }
   led.show();
   delay(1000);
-  /*
-    Xây dựng cơ chế xử lý của bạn tại đây
-  */
 }
