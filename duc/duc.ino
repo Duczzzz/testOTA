eb1ae60a2eeaf8cee5007d3a519e427139065d65
Minh Duc <ducpt423ac@gmail.com>
2026-07-03 19:56:12 +0700

    build-1783083274995:duc

--- duc/duc.ino
+++ duc/duc.ino
@@ -9,6 +9,7 @@
 #include <Update.h>
 #include <Adafruit_BME280.h>
 #include <ESP32Servo.h>
+#include <DHT.h>
 
 const char* ssid = "DUC";
 const char* pass = "14042004";
@@ -42,10 +43,15 @@ FirebaseConfig config;
 
 int checkupdate = 0;
 int demwf = 0;
+
+#define DHTPIN 11
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
 void getupdate()
 {
     display.setTextColor(SSD1306_WHITE);
-    Firebase.setInt(fbdo, "/users/duc/updateOTA",0);
+    Firebase.setInt(fbdo, "/users/duc/updateOTA",0);  
     Serial.print("Firmware URL: ");
     Serial.println(firmwareUrl);
     HTTPClient http;
@@ -77,39 +83,44 @@ void getupdate()
               display.setCursor(0,20);
               display.print(percent);
               display.print("%");
-              display.drawRect(0, 30, 100, 10, SSD1306_WHITE);
-              display.fillRect(2, 32, (percent * 96) / 100, 6, SSD1306_WHITE);
+              display.drawRect(0, 30, 120, 10, SSD1306_WHITE);
+              display.fillRect(
+                    2,
+                    32,
+                    (percent * 116) / 100,
+                    6,
+                    SSD1306_WHITE);
               display.display();
           });
           size_t written = Update.writeStream(client);
           display.clearDisplay();
-          if (written == Update.size()) {
-            Serial.println("Update ghi du du lieu.");
-            if (Update.end()) {
-              if (Update.isFinished()) {
-                Serial.println("Update thanh cong. Dang khoi dong lai...");
-                display.clearDisplay();
-                display.setCursor(0, 10);
-                display.print("Update thanh cong");
-                display.setCursor(0, 30);
-                display.print("Rebooting...");
-                display.display();
-                delay(1000);
-                ESP.restart();
-              } else {
-                Serial.println("Update chua hoan tat!");
-              }
-            } else {
-              Serial.print("Update failed: ");
-              Serial.println(Update.errorString());
-            }
+      if (written == Update.size()) {
+        Serial.println("Update ghi du du lieu.");
+        if (Update.end()) {
+          if (Update.isFinished()) {
+            Serial.println("Update thanh cong. Dang khoi dong lai...");
+            display.clearDisplay();
+            display.setCursor(0, 10);
+            display.print("Update thanh cong");
+            display.setCursor(0, 30);
+            display.print("Rebooting...");
+            display.display();
+            delay(1000);
+            ESP.restart();
           } else {
-            Serial.println("Ghi firmware bi thieu du lieu!");
+            Serial.println("Update chua hoan tat!");
           }
         } else {
-          Serial.println("Khong the bat dau OTA!");
+          Serial.print("Update failed: ");
+          Serial.println(Update.errorString());
         }
+      } else {
+        Serial.println("Ghi firmware bi thieu du lieu!");
+      }
     } else {
+      Serial.println("Khong the bat dau OTA!");
+    }
+    }else {
       Serial.print("Tai firmware that bai. HTTP code: ");
       Serial.println(httpCode);
     }
@@ -117,122 +128,57 @@ void getupdate()
 }
 
 void setup() {
-    /* Người dùng build code tại đây */
-    I2C_BME.begin(8,18);
-    I2C_OLED.begin(13,12);
-    led.begin();
-    led.setBrightness(50);
+  /*
+    Người dùng build code tại đây
+  */
+  dht.begin();
+  I2C_BME.begin(8,18);
+  I2C_OLED.begin(13,12);
+  led.begin();
+  led.setBrightness(50);
+  led.setPixelColor(0, led.Color(255, 0, 255));
+  led.show();  
+  if (!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
+    led.setPixelColor(0, led.Color(255, 0, 0));
+    led.show();
+    Serial.println("OLED fail!");
+    while (1);
+  }
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+  myservo.setPeriodHertz(50);
+  myservo.attach(servoPin, 1000, 2000);
+  display.clearDisplay();
+  display.setCursor(25, 30);
+  display.print("NUKEDASHBOARD");
+  if (!bme.begin(0x76,&I2C_BME)) {
+    display.clearDisplay();
+    Serial.println("Không tìm thấy BME280!");
+    display.setCursor(0, 0);
+    display.printf("Khong tim thay BME280!");
+    display.display();
+    led.setPixelColor(0, led.Color(255, 0, 0));
+    led.show();
+    while (1);
+  }
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.display();
+  delay(1000);
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,0);
+  Serial.begin(115200);
+  Serial.println("He thong dang khoi dong...");
+  display.display();
+  display.clearDisplay();
+  WiFi.begin(ssid,pass);
+  while (WiFi.status() != WL_CONNECTED) {
     led.setPixelColor(0, led.Color(255, 0, 255));
     led.show();
-    if (!display.begin(SSD1306_SWITCHCAPVCC, i2c_Address)) {
-        led.setPixelColor(0, led.Color(255, 0, 0));
-        led.show();
-        Serial.println("OLED fail!");
-        while (1);
-    }
-    ESP32PWM::allocateTimer(0);
-    ESP32PWM::allocateTimer(1);
-    ESP32PWM::allocateTimer(2);
-    ESP32PWM::allocateTimer(3);
-    myservo.setPeriodHertz(50);
-    myservo.attach(servoPin, 1000, 2000);
-    display.clearDisplay();
-    display.setCursor(25, 30);
-    display.print("NUKEDASHBOARD");
-    if (!bme.begin(0x76,&I2C_BME)) {
-        display.clearDisplay();
-        Serial.println("Không tìm thấy BME280!");
-        display.setCursor(0, 0);
-        display.printf("Khong tim thay BME280!");
-        display.display();
-        led.setPixelColor(0, led.Color(255, 0, 0));
-        led.show();
-        while (1);
-    }
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.display();
-    delay(1000);
-    pinMode(LED,OUTPUT);
-    digitalWrite(LED,0);
-    Serial.begin(115200);
-    Serial.println("He thong dang khoi dong...");
-    display.display();
-    display.clearDisplay();
-    WiFi.begin(ssid,pass);
-    while (WiFi.status() != WL_CONNECTED) {
-        led.setPixelColor(0, led.Color(255, 0, 255));
-        led.show();
-        Serial.println("dang khoi dong WiFi...");
-        display.setCursor(10,0);
-        display.print("Dang ket noi WiFi");
-        display.setCursor(0,20);
-        display.printf("SSID: %s",ssid);
-        if(demwf < 80) {
-            display.setCursor(demwf,30);
-            display.print(".");
-            Serial.println(".");
-        } else if(demwf > 80) {
-            display.clearDisplay();
-            demwf = 0;
-        }
-        demwf+=5;
-        display.display();
-        digitalWrite(0,1);
-        delay(300);
-    }
-    digitalWrite(0,0);
-    Serial.printf("Firebase Client v%s\n", FIREBASE_CLIENT_VERSION);
-    config.database_url = DATABASE_URL;
-    config.signer.tokens.legacy_token = DATABASE_SECRET;
-    Firebase.reconnectWiFi(true);
-    fbdo.setBSSLBufferSize(512, 512);
-    Firebase.begin(&config, &auth);
-    led.setPixelColor(0, led.Color(0, 255, 0));
-    led.show();
-    display.clearDisplay();
-    display.display();
-    // Đọc giá trị ban đầu và hiển thị
-    float tempInit = bme.readTemperature(); 
-    float humInit = bme.readHumidity(); 
-    if (tempInit > 32) { led.setPixelColor(0, led.Color(255,0,0)); } else { led.setPixelColor(0, led.Color(0,255,0)); } 
-    led.show(); 
-    display.clearDisplay(); 
-    display.setCursor(0,0); 
-    display.print("Temp: "); 
-    display.print(temperature); 
-    display.print(" C"); 
-    display.setCursor(0,10); 
-    display.print("Hum: "); 
-    display.print(humidity); 
-    display.print(" %"); 
-    display.display();
-}
-
-void loop() {
-    if(Firebase.getInt(fbdo, "/users/duc/updateOTA")) checkupdate = fbdo.intData();
-    if(checkupdate == 1) {
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.setCursor(0, 0);
-        display.print("UPDATE OTA");
-        display.display();
-        getupdate();
-    }
-    /* Xây dựng cơ chế xử lý của bạn tại đây */
-    float temperature = bme.readTemperature(); 
-    float humidity = bme.readHumidity(); 
-    if (temperature > 32) { led.setPixelColor(0, led.Color(255,0,0)); } else { led.setPixelColor(0, led.Color(0,255,0)); } 
-    led.show(); 
-    display.clearDisplay(); 
-    display.setCursor(0,0); 
-    display.print("Temp: "); 
-    display.print(temperature); 
-    display.print(" C"); 
-    display.setCursor(0,10); 
-    display.print("Hum: "); 
-    display.print(humidity); 
-    display.print(" %"); 
-    display.display();
-    delay(1000);
-}
+    Serial.println("dang khoi dong WiFi...");
+    display.setCursor(10,0);
+    display.print("Dang ket noi WiFi");
+    display.setCursor(0,20);
+    display.printf("SSID:
